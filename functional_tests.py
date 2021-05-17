3ff71ead0df5388dfba2e8599ca823d912c144b9
Fernando Sanchez <fernando@excudo.com.br>
2021-05-17 16:39:43 -0700

    Primeira parte do teste funcional

--- functional_tests.py
+++ functional_tests.py
@@ -1,83 +1,83 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
-
-#firefoxOptions = webdriver.FirefoxOptions()
-#firefoxOptions.headless=True
-
 
 class NewVsitorTest(unittest.TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-    def tearDown(self):
-        self.browser.quit()
-        
-    def test_can_start_a_list_and_retrieve_it_later(self):
+	def tearDown(self):
+		self.browser.quit()
 
-# Edith ouviu falar que agora a aplicação online de lista de tarefas
+	def test_can_start_a_list_and_retrieve_it_later(self):
+                # Edith ouviu falar que agora a aplicação online de lista de tarefas
 
-# aceita definir prioridades nas tarefas do tipo baixa, média e alta
+                # aceita definir prioridades nas tarefas do tipo baixa, média e alta
 
-# Ela decide verificar a homepage
+                # Ela decide verificar a homepage
+		self.browser.get("http://localhost:8000")
+                # Ela percebe que o título da página e o cabeçalho mencionam
 
- 
-        self.browser.get("http://localhost:8000")
+                # listas de tarefas com prioridade (priority to-do)
 
-# Ela percebe que o título da página e o cabeçalho mencionam
+		self.assertIn('priority to-do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('priority to-do', header_text)
+		
+                # Ela é convidada a inserir um item de tarefa e a prioridade da 
 
-# listas de tarefas com prioridade (priority to-do)
+                # mesma imediatamente
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
- 
-        self.assertIn('priority to-do', self.browser.title)
-        self.fail('Finish the test!')
+                # Ela digita "Comprar anzol" em uma nova caixa de texto
+
+                # e assinala prioridade alta no campo de seleção de prioridades
+		inputbox.send_keys('Comprar anzol')
 
 
-# Ela é convidada a inserir um item de tarefa e a prioridade da 
+                # Quando ela tecla enter, a página é atualizada, e agora
 
-# mesma imediatamente
+                # a página lista "1 - Comprar anzol - prioridade alta"
 
- 
+                # como um item em uma lista de tarefas
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
-# Ela digita "Comprar anzol" em uma nova caixa de texto
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 
-# e assinala prioridade alta no campo de seleção de prioridades
+                # Ainda continua havendo uma caixa de texto convidando-a a 
 
- 
+                # acrescentar outro item. Ela insere "Comprar cola instantâne"
 
-# Quando ela tecla enter, a página é atualizada, e agora
+                # e assinala prioridade baixa pois ela ainda tem cola suficiente
 
-# a página lista "1 - Comprar anzol - prioridade alta"
+                # por algum tempo
+		self.fail('Finish the test!')
 
-# como um item em uma lista de tarefas
+                # A página é atualizada novamente e agora mostra os dois
 
- 
-
-# Ainda continua havendo uma caixa de texto convidando-a a 
-
-# acrescentar outro item. Ela insere "Comprar cola instantâne"
-
-# e assinala prioridade baixa pois ela ainda tem cola suficiente
-
-# por algum tempo
-
- 
-
-# A página é atualizada novamente e agora mostra os dois
-
-# itens em sua lista e as respectivas prioridades
-
- 
-
-# Edith se pergunta se o site lembrará de sua lista. Então
-
-# ela nota que o site gerou um URL único para ela -- há um 
-
-# pequeno texto explicativo para isso.
+                # itens em sua lista e as respectivas prioridades
 
  
 
-# Ela acessa essa URL -- sua lista de tarefas continua lá.
+                # Edith se pergunta se o site lembrará de sua lista. Então
 
+                # ela nota que o site gerou um URL único para ela -- há um 
+
+                # pequeno texto explicativo para isso.
+
+ 
+
+                # Ela acessa essa URL -- sua lista de tarefas continua lá.
 if __name__ == '__main__':
 	unittest.main()
